6d613527236a9d6a7da3c67f8be59ffbd9f01abd
Ravi VAishnav <99575362+RAVI19099@users.noreply.github.com>
2024-04-16 11:56:52 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,108 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Alan",
+        email: "Alan@example.com",
+      },
+      {
+        name: "Bob",
+        email: "Bob@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "Alan",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+      {
+        name: "Ravi",
+        email: "Ravi@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Ravi",
+        email: "Ravi@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anthony",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Dean",
+        email: "Dean@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Jane Doe",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Carol",
+        email: "Carol@example.com",
+      },
+      {
+        name: "Alan",
+        email: "Alan@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "funk",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+];
